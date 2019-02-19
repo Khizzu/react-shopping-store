3da195751508c69956df5e0d83f6de1d672bbcb6
Khizar Ahmed <34829067+Khizzu@users.noreply.github.com>
2019-02-19 22:53:57 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,26 +16,13 @@ https://www.iconfinder.com/Makoto_msk */}
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
         <ul className="navbar-nav align-items-center">
-          <li className="nav-item ml-5">
-            <Link to="/home" className="nav-link">
-              Home
-            </Link>
-          </li>
+  
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
               Products
             </Link>
           </li>
-          <li className="nav-item ml-5">
-            <Link to="/about us" className="nav-link">
-              About us
-            </Link>
-          </li>
-          <li className="nav-item ml-5">
-            <Link to="/contact" className="nav-link">
-              Contact us
-            </Link>
-          </li>
+          
         </ul>
         <Link to="/cart" className="ml-auto">
           <ButtonContainer>
